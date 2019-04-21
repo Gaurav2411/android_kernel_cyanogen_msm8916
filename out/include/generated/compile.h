8c3cf3f103c8911bedeea8b006eaddcd693fa190
Gaurav241 <gaurav.govinda2420@gmail.com>
2019-04-21 16:46:58 +0000

    lettuce: Reborn:Endgame

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#0 SMP PREEMPT Sun Apr 21 16:17:45 UTC 2019"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "ubuntu-s-6vcpu-16gb-sgp1-01"
+#define LINUX_COMPILER "gcc version 8.2.1 20181014 (GNU Toolchain built by KudDroid) "
